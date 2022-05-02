03d32b756e1ff2689943ecd0086aa0c2f16863b2
Lucas Roman <lucasroman@gmail.com>
2022-05-02 12:15:00 -0300

    Web: fix typo in users.store

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,5 @@ Route::get('/users/create', [UserController::class, 'create'])
     ->name('users.create');
 
 // users.store
-Route::post('/users' [UserController::class, 'store'])
+Route::post('/users', [UserController::class, 'store'])
     ->name('users.store');

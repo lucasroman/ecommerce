ec6d878983b69609afef6849ec833f8c6fb06cf4
Lucas Roman <lucasroman@gmail.com>
2022-05-02 11:38:40 -0300

    View user create: add form for create new user
    Add bootstrap CDN.

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,9 +5,35 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Create User</title>
+
+        {{-- Bootstrap CSS --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
     </head>
 
     <body>
-        <h3>Create user</h3>
+        <div class="container">
+            <h3>Create user</h3>
+
+            <form action="#" method="post">
+                @csrf
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="name">
+                </div>
+                <div class="mb-3">
+                    <label for="alias" class="form-label">Alias</label>
+                    <input type="text" class="form-control" id="alias">
+                </div>
+                <div class="mb-3">
+                    <label for="avatar" class="form-label">Avatar</label>
+                    <input type="file" class="form-control" id="avatar">
+                </div>
+                <button type="submit" class="btn btn-primary">Create</button>
+            </form> 
+        </div>
+
+        {{-- Boostrap --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>

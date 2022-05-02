9374756d7852c62a3f16dabb372c45ffde6a31f1
Lucas Roman <lucasroman@gmail.com>
2022-05-02 11:00:10 -0300

    Web: 'users/create' use UserController
    'user/create' return a string from action 'create' of UserController.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users/create', function() {
-    return 'URL create user exist!';
-})->name('users.create');
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');

4d105a381fac5cf2696b4c13cff4476c10d61e39
Lucas Roman <lucasroman@gmail.com>
2022-05-02 10:54:38 -0300

    Web: add route for create user

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/create', function() {
+    return 'URL create user exist!';
+})->name('users.create');

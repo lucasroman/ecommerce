806759f82283cb9d5b69b72b6ba64bc3be8a3f0d
Lucas Roman <lucasroman@gmail.com>
2022-05-05 11:32:09 -0300

    View create: add app layout and fix form
    - Add layout to create view.
    - Add field names
    - Add encrypt type to form (necessary to upload image).

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,39 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Create User</title>
+@section('title', 'Create user')
 
-        {{-- Bootstrap CSS --}}
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+@section('content')
+    <h3>Create user</h3>
 
-    </head>
-
-    <body>
-        <div class="container">
-            <h3>Create user</h3>
-
-            <form action="{{ route('users.store') }}" method="post">
-                @csrf
-                <div class="mb-3">
-                    <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="name">
-                </div>
-                <div class="mb-3">
-                    <label for="alias" class="form-label">Alias</label>
-                    <input type="text" class="form-control" id="alias">
-                </div>
-                <div class="mb-3">
-                    <label for="avatar" class="form-label">Avatar</label>
-                    <input type="file" class="form-control" id="avatar">
-                </div>
-                <button type="submit" class="btn btn-primary">Create</button>
-            </form> 
+    <form action="{{ route('users.store') }}" method="post" 
+        enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" name="name">
         </div>
-
-        {{-- Boostrap --}}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
-</html>
+        <div class="mb-3">
+            <label for="alias" class="form-label">Alias</label>
+            <input type="text" class="form-control" name="alias">
+        </div>
+        <div class="mb-3">
+            <label for="avatar" class="form-label">Avatar</label>
+            <input type="file" class="form-control" name="avatar">
+        </div>
+        <button type="submit" class="btn btn-primary">Create</button>
+    </form>
+    </div>
+@endsection

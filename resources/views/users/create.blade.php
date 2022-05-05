d9e7c92831d03c8b7753084a56feff757fc79379
Lucas Roman <lucasroman@gmail.com>
2022-05-05 12:40:52 -0300

    View create user: add validation errors
    Add old input in text fields.

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -5,16 +5,28 @@
 @section('content')
     <h3>Create user</h3>
 
+    @if ($errors->any()) 
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{ route('users.store') }}" method="post" 
         enctype="multipart/form-data">
         @csrf
         <div class="mb-3">
             <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" name="name">
+            <input type="text" class="form-control" name="name" 
+                value="{{ old('name') }}">
         </div>
         <div class="mb-3">
             <label for="alias" class="form-label">Alias</label>
-            <input type="text" class="form-control" name="alias">
+            <input type="text" class="form-control" name="alias" 
+                value="{{ old('alias') }}">
         </div>
         <div class="mb-3">
             <label for="avatar" class="form-label">Avatar</label>

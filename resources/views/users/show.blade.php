9b95382080988e6732e48a3b4d342a13bab4120e
Lucas Roman <lucasroman@gmail.com>
2022-05-05 13:09:22 -0300

    View show: add name and alias

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,6 +8,10 @@
         </div>
     @endif
     
+    <h3>{{ $user->name }}</h3>
+
+    <h4>"{{ $user->alias }}"</h4>
+    
     {{-- Image to render --}}
     <img src="{{ Storage::url($user->avatar) }}" alt="Avatar image here">
     

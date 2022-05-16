873eb44250785dfff79e552444130e7335974d6f
Lucas Roman <lucasroman@gmail.com>
2022-05-16 11:54:26 -0300

    User show view: add delete user button
    This action require a complete form with the spoof delete.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,5 +14,14 @@
     
     {{-- Image to render --}}
     <img src="{{ Storage::url($user->avatar) }}" alt="Avatar image here">
-    
+
+    <div class="mt-3">
+        <form action="{{ route('users.destroy', $user) }}" method="post">
+            @csrf
+            @method('delete')
+            <button type="submit" class="btn btn-danger">
+                Delete User
+            </button>
+        </form>
+    </div>
 @endsection

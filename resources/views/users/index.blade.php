da5b9285aaab487eddbfeca3e77f96e1ea6db584
Lucas Roman <lucasroman@gmail.com>
2022-05-25 11:43:39 -0300

    Add user list to index view

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,5 +4,19 @@
 
 @section('content')
     <h3>Users</h3>
-    
+    <ul class="list-group list-group-flush">
+    @foreach ($users as $user)
+        
+            <li class="list-group-item">
+                <a href="{{route('users.show', $user)}}">{{ $user->name }}
+                </a>
+                </li>
+    @endforeach
+    </ul>
+
+    <a href="{{ route('users.create') }}">
+        <button class="btn btn-primary">
+            New User
+        </button>
+    </a>
 @endsection

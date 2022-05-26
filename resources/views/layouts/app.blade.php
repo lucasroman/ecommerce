8c9519d645b9a57fcfc60ecdfc6606b6d207dca7
Lucas Roman <lucasroman@gmail.com>
2022-05-26 10:52:56 -0300

    App layout: add Font Awesome 6 Pro by CDN

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,9 @@
 
         {{-- Bootstrap CSS --}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        {{-- Font Awesome 6 Pro --}}
+        <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro-v6@18657a9/css/all.min.css" rel="stylesheet" type="text/css" />
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">

8414fae406d7992134a8a42380660fbddabd44d9
Lucas Roman <lucasroman@gmail.com>
2022-05-26 10:54:06 -0300

    Index view: add avatar image to users list
    Add a 'New User' button too.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,20 +3,25 @@
 @section('title', 'Users list')
 
 @section('content')
-    <h3>Users</h3>
-    <ul class="list-group list-group-flush">
-    @foreach ($users as $user)
-        
-            <li class="list-group-item">
-                <a href="{{route('users.show', $user)}}">{{ $user->name }}
-                </a>
-                </li>
-    @endforeach
-    </ul>
+<h3>Users</h3>
 
-    <a href="{{ route('users.create') }}">
-        <button class="btn btn-primary">
-            New User
-        </button>
-    </a>
+<ul class="list-group list-group-flush">
+    @foreach ($users as $user)
+    <li class="list-group-item">
+        <a href="{{route('users.show', $user)}}">
+            <img src="{{Storage::url($user->avatar)}}" 
+                class="img-thumbnail rounded-circle" 
+                alt="{{ $user->avatar }}" 
+                style="height: 6em">
+            {{ $user->name }}
+        </a>
+    </li>
+    @endforeach
+</ul>
+
+<a href="{{ route('users.create') }}">
+    <button class="btn btn-primary">
+        New User
+    </button>
+</a>
 @endsection

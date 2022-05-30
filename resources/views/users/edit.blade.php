07b4d951bf99f7387f92a3f2e9a7211b104dab3c
Lucas Roman <lucasroman@gmail.com>
2022-05-30 12:06:07 -0300

    Edit view: add remaining fields to form
    - Add name, alias and avatar fiels to edit form.
    - Improvement: ternary opeator to fill fields (except avatar field).
    This feature allow fill fields with data from database or old data
    input by user.
    - Add button to back to users list (index).

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,12 +4,39 @@
     
 @section('content')
     <h3>Edit user</h3>
-    <form action="{{ route('users.update', $user) }}" method="post">
+
+    @if ($errors->any()) 
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+                @if ($errors->has('avatar'))
+                    <li>The avatar dimensions must be 200x200 pixels</li>
+                @endif
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('users.update', $user) }}" method="post" enctype="multipart/form-data">
         @csrf
         @method('patch')
-        <input type="text" name="name" value="{{ old('name')}}" 
-            placeholder="{{ $user->name }}">
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" name="name" 
+                value="{{ old('name') ?: $user->name }}">
+        </div>
+        <div class="mb-3">
+            <label for="alias" class="form-label">Alias</label>
+            <input type="text" class="form-control" name="alias" 
+                value="{{ old('alias') ?: $user->alias }}">
+        </div>
+        <div class="mb-3">
+            <label for="avatar" class="form-label">Avatar</label>
+            <input type="file" class="form-control" name="avatar">
+        </div>
         <button type="submit" class="btn btn-primary">Update</button>
     </form>
-    
+
+    <a href="{{ route('users.index') }}"><button class="btn btn-primary mt-3">Back to User list</button></a>
 @endsection

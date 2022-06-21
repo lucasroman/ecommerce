7feefd6810fd75a32aa8a2c9dea4cd4a5e76e50f
Lucas Roman <lucasroman@gmail.com>
2022-06-21 11:36:06 -0300

    Users Test: add test for users index
    Tests check:
    - Uri and index view must exist.
    - Show users (name, alias and avatar) data in index view.

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,12 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // User instantiated but not saved in the database
+        $this->user = User::create([
+            'name' => 'Jhon Doe', 
+            'alias' => 'The Doe', 
+            'avatar' => 'storage/exampleImage.jpg', 
+        ]);
+    }
+    
     /**
      * A basic feature test example.
      *
@@ -18,5 +33,28 @@ class UserTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    // Users have a route and view
+    public function testUsersResourceExist()
+    {
+        $response = $this->get('/users');
+
+        // Check users route exist
+        $response->assertStatus(200);
+
+        // Check users view exist
+        $response->assertViewIs('users.index');
+    }
+
+    // Users view show all users in database
+    public function testUsersIndexShowAllUsersInDatabase()
+    {
+        $response = $this->get('/users');
+
+        // Check all user data is showed in index view
+        $response->assertSee('Jhon Doe')
+            ->assertSee('The Doe')
+            ->assertSee('storage/exampleImage.jpg');
     }
 }

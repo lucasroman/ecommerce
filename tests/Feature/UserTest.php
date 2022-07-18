01d7e8b50bdb0729be0254e2f58dac6919b985d7
Lucas Roman <lucasroman@gmail.com>
2022-07-18 14:39:53 -0300

    UserTest: add test for new user registration
    - Check create user view exist.
    - Check can create a new user with valid data.

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class UserTest extends TestCase
 {
@@ -57,4 +59,49 @@ class UserTest extends TestCase
             ->assertSee('The Doe')
             ->assertSee('storage/exampleImage.jpg');
     }
+
+    // Create user view exist
+    public function testCreateUserViewMustExist()
+    {
+        $response = $this->get('/users/create');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     *  IMPORTANT: the require "php-gd" extension is required installed 
+     *  to test work.
+     * */ 
+    // New user send valid data
+    public function testCreateUserSendValidData()
+    {
+        // Create a fake avatar image of 200x200 pixels
+        
+        Storage::fake('avatars');
+
+        $file = UploadedFile::fake()->image('avatar.jpg', 200, 200);
+
+        // Post valid data
+        
+        $response = $this->post('/users', [
+            'name' => 'Example name', 
+            'alias' => 'Example alias',     
+            'avatar' => $file, 
+        ]);
+
+        // Get id of user newly created (above)
+
+        $newUser = User::all()->last();
+     
+        // Check redirect to show view of new user (this count like 2 asserts)
+
+        $response->assertRedirect('/users/'. $newUser->id);
+
+        // Check that new user exists in database
+        
+        $this->assertModelExists($newUser);
+    }
+
+    // 2. Edit
+    // 3. Show
 }

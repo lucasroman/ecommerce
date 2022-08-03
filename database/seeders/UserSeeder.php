86d89dee7babc2e228aa1b1329a896469ad4a3c1
Lucas Roman <lucasroman@gmail.com>
2022-08-02 21:39:04 -0300

    UserSeeder: add email field to fix record
    Also explain two way for truncate users table.
    
    1. Using a query builder and;
    2. Using a model itself.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,16 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+/* You can truncate tables in two ways:
+1. Use query builder:
+    use Illuminate\Support\Facades\DB; <-- You need add DB facade
+    DB::table('users')->truncate();
+
+2. Use model of table to truncate:
+    use App\Models\User; <-- You need add the model!
+    User::truncate();
+ */
+
 // Truncate table before to seed
 DB::table('users')->truncate();
 
@@ -22,6 +32,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'John Doe', 
             'alias' => 'Fulgore', 
+            'email' => 'jdoe@example.com', 
             'avatar' => 'No image file',
         ]);
     }

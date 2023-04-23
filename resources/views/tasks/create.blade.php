fbdb3276957ac46b5adb30f40403e724aa081960
Lucas Roman <lucasroman@gmail.com>
2023-04-23 00:02:10 -0300

    Task create form: update
    Add form for create a new task.

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -1,1 +1,23 @@
-Create Task view working on.
+<h1>Create Task</h1>
+
+<form method="POST" action={{ route('tasks.store') }}>
+    @csrf
+
+    <div>
+        <input id="done" type="checkbox">
+        <label for="done">Done</label>
+    </div>
+
+    <div>
+        <label for="title">Title</label>
+        <input id="title" type="text">
+    </div>
+
+    <div>
+        <lable for="description">Description</lable>
+        <textarea id="description" name="description" rows="4" cols="50">
+        </textarea>
+    </div>
+
+    <input type="submit" value="Create">
+</form>

6c46b875924a7a8334b6887a0c61d7ffea17565b
Lucas Roman <lucasroman@gmail.com>
2023-04-23 23:01:50 -0300

    TaskController: add store action

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -34,7 +35,9 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Task::create($request->all());
+
+        return redirect()->route('tasks.create');
     }
 
     /**

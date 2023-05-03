565be3609e4cc626cb009b3efc3d039f1ff29151
Lucas Roman <lucasroman@gmail.com>
2023-05-02 21:49:45 -0300

    Task index: add edit button for each task

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,7 +8,10 @@
     <h3>Title: {{ $task->title }}</h3>
 
     <h3>Description: {{ $task->description }}</h3>
-    
+
+    {{-- Edit task button --}}
+    <input type="button" class="btn btn-primary" onclick="location.href=
+        '{{ route('tasks.edit', $task->id) }}'" value="Edit JS" />
     <hr>
 @endforeach
 

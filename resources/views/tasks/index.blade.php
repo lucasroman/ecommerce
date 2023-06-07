7c591760333c226a4d7f14f12eacd45104c5ee84
Lucas Roman <lucasroman@gmail.com>
2023-06-07 11:21:55 -0300

    Task list (index) update
    Replace card by tasks list with icons to develop different actions.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,35 +4,31 @@
 
 <h1>All Tasks</h1><hr>
 
-<div class="d-flex container-fluid">
+
+<ul class="list-group">
 @foreach ($tasks as $task)
-    <div class="card me-3" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">{{$task->title}}</h5>
+    <li class="list-group-item">
+        <h2>
+        {{$task->title}}
+        <a class=" text-decoration-none" href="{{route('tasks.show', $task)}}">
+            <i class="far fa-eye" style="color: #0080ff;"></i>
+        </a>
+        <a class=" text-decoration-none" href="{{route('tasks.edit', $task)}}">
+            <i class="fas fa-edit" style="color: #0080ff;"></i>
+        </a>
 
-            <h6 class="card-subtitle mb-2 text-body-secondary">
-                {{$task->done ? 'Done' : 'Incomplete'}}
-            </h6>
-
-            <p class="card-text">{{$task->description}}</p>
-
-            {{-- Edit task button --}}
-            <div class="d-flex gap-2 ms-3">
-            <a class="btn btn-primary rounded-pill px-3" 
-                href="{{route('tasks.edit', $task->id)}}">Edit</a>
-
-            {{-- Delete task button --}}
-            <form method="POST" action="{{route('tasks.destroy', $task->id)}}">
-                @csrf
-                @method('DELETE')
-                <input class="btn btn-primary rounded-pill px-3" 
-                onclick="return confirm('Are you sure you want to delete this task?')" type="submit" value="Delete">
-            </form>
-            </div>
-        </div>
-        </div>
-        
-@endforeach
-</div>
+        <form method="POST" action={{route('tasks.destroy', $task)}}>
+            @csrf
+            @method('DELETE')
+            <input type="submit">
+            <a class=" text-decoration-none" type="submit"
+            href="{{route('tasks.destroy', $task)}}">
+            <i class="fas fa-trash-alt" style="color: #0080ff;"></i>
+        </a>
+        </form>
+        </h2>
+    </li>
+    @endforeach
+</ul>
 
 @endsection

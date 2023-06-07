33c0439101d181ea63991e1ca91bcbd61430c4ba
Lucas Roman <lucasroman@gmail.com>
2023-06-07 11:20:30 -0300

    Add Font Awesome 5.15.4
    Add button to index tasks in app layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,10 @@
 
     {{-- Bootstrap 5.3 CSS --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"></head>
-    
+
+    {{-- Font Awesome 5.15.4 --}}
+    <link href="https://cdn.jsdelivr.net/gh/hung1001/font-awesome-pro@4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
+
 <body class="container">
     <div class="ms-3 mt-3">
         <a class="btn btn-warning" href="{{route('home')}}">
@@ -17,6 +20,10 @@
         
         <a class="btn btn-warning" href="{{route('tasks.create')}}">
             Create Task
+        </a>
+
+        <a class="btn btn-info" href="{{route('tasks.index')}}">
+            Tasks list
         </a>
     </div>
 

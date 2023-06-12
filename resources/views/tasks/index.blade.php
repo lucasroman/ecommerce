8e8118ca28e426a5a7f7ade7c98e71b3030b5cde
Lucas Roman <lucasroman@gmail.com>
2023-06-11 22:51:17 -0300

    Align action icons and add background color
    To align all icons in a row all are inside of delete form.
    Add background color to task depending of the task is complete or not.
    Change text color to white to improve visualization.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,24 +7,34 @@
 
 <ul class="list-group">
 @foreach ($tasks as $task)
-    <li class="list-group-item">
+    <li class="list-group-item  bg-{{$task->done ? 'success': 'danger'}}
+        text-light">
+
         <h2>
         {{$task->title}}
-        <a class=" text-decoration-none" href="{{route('tasks.show', $task)}}">
-            <i class="far fa-eye" style="color: #0080ff;"></i>
-        </a>
-        <a class=" text-decoration-none" href="{{route('tasks.edit', $task)}}">
-            <i class="fas fa-edit" style="color: #0080ff;"></i>
-        </a>
+        
+        {{-- Show and Edit icons doesn't need be inside of form 
+        but I put in for align them --}}
 
         <form method="POST" action={{route('tasks.destroy', $task)}}>
             @csrf
             @method('DELETE')
-            <input type="submit">
-            <a class=" text-decoration-none" type="submit"
-            href="{{route('tasks.destroy', $task)}}">
-            <i class="fas fa-trash-alt" style="color: #0080ff;"></i>
-        </a>
+            
+            <a class=" text-decoration-none" 
+                href="{{route('tasks.show', $task)}}">
+                <i class="far fa-eye" style="color: white;"></i>
+            </a>
+
+            <a class="text-decoration-none" 
+                href="{{route('tasks.edit', $task)}}">
+                <i class="fas fa-edit" style="color: white"></i>
+            </a>
+
+            <button class="btn-lg" type="submit" 
+                style="background-color: transparent; border:none;" 
+                href="{{route('tasks.destroy', $task)}}">
+                <i class="fas fa-trash-alt" style="color: white"></i>
+            </button>
         </form>
         </h2>
     </li>

f83c457b6a0d4c826f1ae588fd1a0cb8074e9267
Lucas Roman <lucasroman@gmail.com>
2023-07-11 19:45:38 -0300

    Change timezone to Buenos Aires (Argentina)

--- config/app.php
+++ config/app.php
@@ -69,7 +69,9 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    // 'timezone' => 'UTC',
+
+    'timezone' => 'America/Argentina/Buenos_Aires',
 
     /*
     |--------------------------------------------------------------------------

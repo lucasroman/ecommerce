add469ed34454a2e9976ab0dfd09aef0c193b5f9
Lucas Roman <lucasroman@gmail.com>
2023-07-13 22:03:39 -0300

    Add routes for update user status
    - Change root to user index.
    - New route for change user status.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [UserController::class, 'index']);
+// 
+// Update user state but using AJAX so dosen't need use post verb
+Route::get('/status/update', [UserController::class, 'updateStatus'])
+    ->name('users.update.status');

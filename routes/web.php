238fda83ac43651a521f1456a27a2bc2d8c49f98
Lucas Roman <lucasroman@gmail.com>
2023-09-24 00:12:27 -0300

    Update user's routes
    Use ProfileController instead of UserController in users routes.

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,22 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
     Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 });
 
 require __DIR__.'/auth.php';
 
-Route::resource('/users', UserController::class);
+Route::get('/dashboard/users/create', [UserController::class, 'create'])
+    ->name('users.create');
+
+Route::post('/dashboard/users', [UserController::class, 'store'])
+    ->name('users.store');
+
+Route::get('/dashboard/users/edit', [ProfileController::class, 'edit'])
+    ->name('user.edit');

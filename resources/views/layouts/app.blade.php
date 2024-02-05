99dbdb7d484af5b8eb977062a628a05109f78df7
Lucas Roman <lucasroman@gmail.com>
2024-02-05 09:17:27 -0300

    Add Font Awesome 6.4.2 to 'app' layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,25 @@
         
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        {{-- Font Awesome 6 --}}
+        {{-- Note: for get the links below you have to go to Font Awesome home Page, do right click and select "View page source". Find and copy all links in that page and paste it here. Enjoy!. --}}
+
+        <link rel="stylesheet" data-purpose="Layout StyleSheet" title="Web Awesome" href="/css/app-wa-02670e9412103b5852dcbe140d278c49.css?vsn=d">
+
+        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/all.css">
+
+        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/sharp-solid.css">
+
+        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/sharp-regular.css">
+
+        <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.2/css/sharp-light.css">
+
+        {{-- <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}"> --}}
+        <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+
+
+        
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">

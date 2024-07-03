8d0f42727a1c81cc8d677f3b06fc2220338c0233
Lucas Roman <lucasroman@gmail.com>
2024-07-02 23:18:19 -0300

    Add user list on React version
    Note: the swtichs aren't working yet. It's
    necessary add a form in 'status' input.

--- resources/js/components/UsersList.jsx
+++ resources/js/components/UsersList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 const app = document.querySelector("#app");
 const name = app.dataset.name;
@@ -7,12 +8,52 @@ const name = app.dataset.name;
 const users = JSON.parse(app.dataset.users);
 
 export default function UserList() {
+  const [check, setCheck] = useState(false);
+  
   return (
     <>
-      <h1>Test component working!!!!! </h1>
-      <ul>
-        {users.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul>
+      <main className="py-4">
+      <div className="container">
+      <div className="row justy-content-center">
+      <div className="col-md-12">
+
+      <div className="card">
+        <div className="card-header">User List</div>
+        <div className="card-body">
+          <table className="table table-hover table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Status</th>
+                <th scope="col">Created</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map(user =>
+                <tr>
+                  <th>{user.id}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <input type="checkbox"
+                    dataId={user.id}
+                    className="js-switch"
+                    onClick={() => setCheck(!check)}
+                    />
+                  </td>
+                  <td>{user.created_at}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      </main>
     </>
   );
 }

9703e7c32f98a44e0c0e7439063fc67434004537
Lucas Roman <lucasroman@gmail.com>
2024-07-24 15:04:40 -0300

    Services list: add case 'there are not service'
    Add button to publish a new services in case of
    user have not anyone.

--- resources/views/profile/services.blade.php
+++ resources/views/profile/services.blade.php
@@ -9,13 +9,21 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
+
           <ul>
-            @forelse (Auth::user()->services->pluck('name') as $service)
-              <li>{{$service}}</li>
+            @forelse (Auth::user()->services as $service)
+              <a href="{{ route('service', $service) }}">
+                <li>{{$service->name}}</li>
+              </a>
             @empty
+
             There are not services yet. Why don't add one?
+            <a href="#">
+              <button>Add new service</button>
+            </a>
             @endforelse
           </ul>
+
         </div>
       </div>
     </div>

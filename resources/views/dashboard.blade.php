db2204a0920857d35c5f1bfbf1b2bfd1e96b0b48
Lucas Roman <lucasroman@gmail.com>
2024-07-24 14:27:57 -0300

    Remove user constraint
    Now all user can see their services list.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,9 +10,9 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
-                    @if (Auth::user()->name === 'Luke')
+                    
                         <div id="app">ReactJS Entry Point Here</div>
-                    @endif
+                    
                 </div>
             </div>
         </div>

89cd836f7889adc8415a22ca7cbdd322c7bc8a1c
Lucas Roman <lucasroman@gmail.com>
2024-08-05 23:28:26 -0300

    Complete service view
    Add:
    - CSS
    - Image service
    - Service description
    - Chat button

--- resources/views/profile/service.blade.php
+++ resources/views/profile/service.blade.php
@@ -3,7 +3,20 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100">
-          <h1>{{$service->name}}</h1>
+          <h1 class="text-2xl mb-4">{{$service->name}}</h1>
+
+          <div class="flex">
+            <div class="flex-none mr-4">
+              <img src="{{asset($service->image)}}" alt="image service">
+            </div>
+
+            <div class="" style="margin-top:-7px">
+              <p>{{$service->description}}</p>
+              <x-primary-button id="submit" class="mt-4 items-center">
+                {{ __('Chat with '. $service->user->name) }}
+          </x-primary-button>    
+            </div>            
+          </div>      
         </div>
       </div>
     </div>

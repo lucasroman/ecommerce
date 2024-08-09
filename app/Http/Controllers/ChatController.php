9f1ef374a35f890ce9e23e3c9d917c9f64248d2a
Lucas Roman <lucasroman@gmail.com>
2024-08-09 19:36:25 -0300

    Create ChatController
    Change create and store actions.

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Service;
+use App\Models\Chat;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Service $service)
+    {
+        return view('profile.chat', compact('service'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $chat = new Chat([
+            'owner' => 'Test owner',
+            'guest' => 'Test guest',
+            'message' => 'Hi, this is a test message',
+        ]);
+        // owner, guest, message   
+        $chat->save();
+        return response()->json($chat);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chat $chat)
+    {
+        //
+    }
+}

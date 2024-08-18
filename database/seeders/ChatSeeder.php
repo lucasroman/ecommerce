6145fa8558249f837334a6f8212bdfdb459796c2
Lucas Roman <lucasroman@gmail.com>
2024-08-17 23:35:23 -0300

    Create ChatSeeder
    Add ChatSeeder to DatabaseSeeder too.

---
+++ database/seeders/ChatSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Chat;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ChatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Tuncate chats table
+        Chat::truncate();
+    }
+}

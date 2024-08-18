37bc0844aacbc06e094edd748a561f93004dd5f8
Lucas Roman <lucasroman@gmail.com>
2024-08-17 23:39:35 -0300

    Update service list view
    Use variables that coming from ServiceController for iterate and show services.

--- resources/views/profile/services-list.blade.php
+++ resources/views/profile/services-list.blade.php
@@ -9,35 +9,38 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 text-gray-900 dark:text-gray-100 columns-2">
-          <h1 class="text-2xl">My services</h1>
-          <hr>
 
+          <h1 class="text-2xl">My services</h1><hr>
           <ul>
-            @forelse (Auth::user()->services as $service)
-              <a href="{{ route('service', $service) }}">
-                <li>{{$service->name}} <i>By {{$service->user->name}}.</i></li>
-              </a>
+            @forelse ($servicesOfOwner as $service)
+              <li>
+                <a href="#">
+                  {{$service->name}} <i>By {{$service->user->name}}.</i>
+                </a>
+              </li>
+          
             @empty
-
-            There are not services yet. Why don't add one?
-            <a href="#">
-              <button>Add new service</button>
-            </a>
+              There are not services yet. Why don't add one?.
+              <a href="#">
+                <button>Add new service</button>
+              </a>
             @endforelse
           </ul>
 
-          <h1 class="text-2xl mt-1.5">Community services</h1>
-          <hr>
+          {{-- ---------------------------------------------------------- --}}
 
+          <h1 class="text-2xl mt-1.5">Community services</h1><hr>
           <ul>
-            @forelse(App\Models\Service::all() as $service)
-              @if ($service->user->email != Auth::user()->email)
-                <a href="{{ route('service', $service) }}">
-                <li>{{$service->name}} <i>By {{$service->user->name}}.</i></li>
+            @forelse($servicesOfOthers as $service)
+              <li>
+                <a href="#">
+                  {{$service->name}} <i>By {{$service->user->name}}.</i>
                 </a>
-              @endif
+              </li>
             @empty
+              No one has published any services yet.
             @endforelse
+
           </ul>
         </div>
       </div>

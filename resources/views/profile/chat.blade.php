678ceefefd63f606960aef010711631128b5836a
Lucas Roman <lucasroman@gmail.com>
2024-09-06 19:24:01 -0300

    Update chat form
    - Align messages depending of how is talking.
    - Fix overlap messages.
    - Fix responsive input text.
    - Change chat colors of backgraund and messages.

--- resources/views/profile/chat.blade.php
+++ resources/views/profile/chat.blade.php
@@ -7,34 +7,43 @@
             (by {{ $service->user->name }})</h1>
         </div>
         
-        {{-- Show history chat messages --}}
-        @foreach ($chat as $msg)          
-        <div class="text-white">
-            
-            @if ($msg->speaker)
-              <div class="size-fit rounded-lg mb-2 p-2 bg-amber-800 shadow-lg">
-                <p class="font-bold">{{$guest->name}}</p>{{$msg->message}}
-              </div>
-            @else
-              <div class="size-fit rounded-lg mb-2 p-2 bg-lime-800 shadow-lg text-right">
-                <p class="font-bold">{{$owner->name}}</p>{{$msg->message}}
-              </div>
-            @endif
-          </div>  
-        @endforeach
-        
-        {{-- Input chat message --}}
-        <form action="{{route('chats.store')}}" method="post">
-          @csrf
-          <input type="hidden" name="serviceId" value="{{$service->id}}">
-          <input type="hidden" name="owner" value="{{$owner->id}}">
-          <input type="hidden" name="guest" value="{{$guest->id}}">
-          <input type="text" name="message">
-          <input type="hidden" name="speaker" 
-            value="{{(Auth::user()->id === $service->user_id) ? 0 : 1}}">
-          <x-primary-button type="submit">Send</x-primary-button>
-        </form>
+        <div class="m-3 w-1/3 bg-slate-500 rounded-lg p-4 grid ">            
+          {{-- Show history chat messages --}}
+          @foreach ($chat as $msg)          
+              @if ($msg->speaker)
+                <div class="size-fit rounded-lg mb-2 p-2 bg-slate-300 shadow-xl text-black clear-end">
+                  
+                  <p><b>{{$guest->name}}</b> 
+                    ({{($msg->created_at->diffForHumans())}})</p>{{$msg->message}}
+                </div>
+              @else
+                <div class="size-fit rounded-lg mb-2 p-2 bg-indigo-900 shadow-xl text-right text-white grid justify-self-end">
+                  <p><b>{{$owner->name}}</b> 
+                    ({{$msg->created_at->diffForHumans()}})</p>
+                    {{$msg->message}}
+                </div>
+                @endif
+          @endforeach
+          
+          {{-- Input chat message --}}
+
+          <form action="{{route('chats.store')}}" method="post">
+            @csrf
+            <input type="hidden" name="serviceId" value="{{$service->id}}">
+            <input type="hidden" name="owner" value="{{$owner->id}}">
+            <input type="hidden" name="guest" value="{{$guest->id}}">
+            <input type="hidden" name="speaker" 
+              value="{{(Auth::user()->id === $service->user_id) ? 0 : 1}}">
+            <div class="text-center flex">
+              <input type="text" name="message" class="rounded-lg block w-full">
+              <x-primary-button type="submit" class="ml-3 clear-end ">
+                  Send
+              </x-primary-button>
+            </div>
+          </form>
+
+        </div>
       </div>
-    </div>
+    </div>  
   </div>
 </x-app-layout>
